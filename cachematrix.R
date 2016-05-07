61ba473932edd3af898bfc5f9775ab40168e5710
JDDATA <rebecca.pluxo@gmail.com>
2016-05-07 17:43:08 +0800

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following two functions will calculate the inverse matrix or retrieve the inverse matrix from the cache
+## The "makeCacheMatrix" function allows caching of its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  IV <- NULL
+  set <- function(y) {
+    x <<- y
+    IV <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) IV <<- inverse
+  getInverse <- function() IV
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+## Write a short comment describing this function: computes the inverse of the special “matrix” (which is the input of cachemean) returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       IV <- x$getInverse()
+       if(!is.null(IV)){
+         message("getting cached data")
+         return(IV)
+       }
+       mat <- x$get()
+       IV <- solve(mat, ...)
+       x$setInverse(IV)
+       IV
 }
